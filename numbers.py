3c2777ccab3c6d38c86873f33292210adca25db1
Muhammad Riaz <riaz@Muhammads-MBP.hub>
2021-12-13 14:38:12 +1300

    Until lists

---
+++ numbers.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+str1 = "Value"
+n = 10
+numbers = [1, 2, 3,  4, 5,  6, 7,  8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+              20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 
+              38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+
+print("Start time is: " + str(datetime.now()))
+
+for i in range(10):
+    j = i%2
+    if j > 0:
+        print("I am Odd = " + str(i))
+    else:
+        print("I am Even = " + str(i))
---
+++ strings.py
@@ -0,0 +1,61 @@
+str1 = 'Hello World'
+str2 = "Greetings FROM Canada"
+str3 = """This string is enclosed in three quotation marks
+and multiple lines can be written into this 
+string.
+"""
+
+
+#print(str1)
+
+#print(str2)
+
+#print(str3)
+
+#print(str2[-6])
+
+#print(len(str1))
+
+#for x in str1:
+#    print(x)
+
+
+#b = "is" not in str3
+
+#print(b)
+
+#str4 = str1.replace("o", "p")
+#print(str4)
+
+#print(str1[0])
+
+#list1 = str1.split(" ")
+#print(len(list1))
+#print(list1[0])
+#print(list1[1])
+
+#str4 = str1 + " " + 12
+
+#age = 20
+#txt = "His name is Albert, and he is {} years old"
+
+#quantity = 2
+#price = 30
+#myorder = "I want {1} pizzas for {0} dollars."
+
+
+#print(myorder.format(price, quantity))
+
+
+#saying = 'Learning is an active process,we learn by doing. Only knowledge that is used sticks in your mind. \'By Dale Carnegie\''
+#print(saying)
+
+val = "Hello, Python!"
+print(val[2:5])
+
+#print(val[:6])
+
+#print(val[6:])
+
+print(val[-5:-2])
+
